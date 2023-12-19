32c59c7e06e0ea937a65605f6e043f1c5dfa891e
Sebastian Suaza <SsuazaA@gmail.com>
2023-12-19 11:30:05 -0500

    feat(coverage): Increment coverage
    Remove @NoArgsConstructor in Problem and ProblemCase classes

--- src/main/java/cloud/inclusion/test/domain/model/Problem.java
+++ src/main/java/cloud/inclusion/test/domain/model/Problem.java
@@ -5,11 +5,9 @@ import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Builder(toBuilder = true)
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class Problem {
 
--- src/main/java/cloud/inclusion/test/domain/model/ProblemCase.java
+++ src/main/java/cloud/inclusion/test/domain/model/ProblemCase.java
@@ -3,11 +3,9 @@ package cloud.inclusion.test.domain.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Builder(toBuilder = true)
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class ProblemCase {
 
